86305ad9b85e9ad39d9d3b25b39d7c41994f27d7
Leo 🦁 <leo@mojgazda.com>
2026-02-15 20:17:50 +0100

    🔥 RADICAL 90s GEOCITIES REDESIGN 🔥
    - Framesets with navigation sidebar
    - Tables with BORDER=1 everywhere
    - Web-safe neon colors (hot pink, lime, cyan)
    - Tiled starfield backgrounds
    - Comic Sans MS + Times New Roman
    - MARQUEE scrolling welcome
    - BLINK tags for deals
    - Animated GIFs: flames, globes, construction
    - Hit counter, Netscape badges, Guestbook
    - Webring navigation
    
    COWABUNGA DUDE!!!

---
+++ js/listings.js
@@ -0,0 +1,79 @@
+window.mtbData = {
+  "listings": [
+    {
+      "id": 1,
+      "brand": "Klein",
+      "title": "Klein Attitude Race - Sea & Sky",
+      "price": 1290,
+      "marketValue": 2500,
+      "dealScore": 48,
+      "dealType": "great",
+      "location": "Slovenia",
+      "country": "si",
+      "isGrail": true,
+      "isVintage": true,
+      "sourceUrl": "#",
+      "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Klein_Attitude_Gossomer_Paint%3B_2008%2C_03._09.%2C_02.JPG/500px-Klein_Attitude_Gossomer_Paint%3B_2008%2C_03._09.%2C_02.JPG"
+    },
+    {
+      "id": 2,
+      "brand": "GT",
+      "title": "GT Zaskar LE 1994 - Original",
+      "price": 850,
+      "marketValue": 1200,
+      "dealScore": 29,
+      "dealType": "good",
+      "location": "Austria",
+      "country": "at",
+      "isGrail": false,
+      "isVintage": true,
+      "sourceUrl": "#",
+      "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/GT_Zaskar_2012.JPG/500px-GT_Zaskar_2012.JPG"
+    },
+    {
+      "id": 3,
+      "brand": "Specialized",
+      "title": "Stumpjumper M2 1991 - Full XT",
+      "price": 400,
+      "marketValue": 700,
+      "dealScore": 43,
+      "dealType": "great",
+      "location": "Slovenia",
+      "country": "si",
+      "isGrail": false,
+      "isVintage": true,
+      "sourceUrl": "#",
+      "imageUrl": "images/stumpjumper-m2.jpg"
+    },
+    {
+      "id": 4,
+      "brand": "Ritchey",
+      "title": "Ritchey P-23 1989 - Collector",
+      "price": 1450,
+      "marketValue": 2000,
+      "dealScore": 28,
+      "dealType": "good",
+      "location": "Slovenia",
+      "country": "si",
+      "isGrail": true,
+      "isVintage": true,
+      "sourceUrl": "#",
+      "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Historical_Tom_Ritchey_Bicycles_%2817359667581%29.jpg/500px-Historical_Tom_Ritchey_Bicycles_%2817359667581%29.jpg"
+    },
+    {
+      "id": 5,
+      "brand": "Kuwahara",
+      "title": "Kuwahara Cascade 1988 - Barn Find",
+      "price": 320,
+      "marketValue": 500,
+      "dealScore": 36,
+      "dealType": "great",
+      "location": "Croatia",
+      "country": "hr",
+      "isGrail": false,
+      "isVintage": true,
+      "sourceUrl": "#",
+      "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Sepeda_gunung_vintage.jpg/500px-Sepeda_gunung_vintage.jpg"
+    }
+  ]
+};
